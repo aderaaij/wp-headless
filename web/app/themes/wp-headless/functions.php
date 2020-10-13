953a572513cc41a91cfa3c99eb102c57ff941d58
Arden de Raaij <a.de.raaij@gmail.com>
2020-10-13 21:31:37 +0100

    unset image sizes

--- web/app/themes/wp-headless/functions.php
+++ web/app/themes/wp-headless/functions.php
@@ -1,3 +1,10 @@
 <?php 
 
   add_theme_support( 'post-thumbnails' );
+  
+  function remove_default_image_sizes( $sizes) {   
+    unset( $sizes['1536x1536']);
+    unset( $sizes['2048x2048']);
+    return $sizes;
+  }
+  add_filter('intermediate_image_sizes_advanced', 'remove_default_image_sizes');
